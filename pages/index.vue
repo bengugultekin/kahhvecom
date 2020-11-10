1b4854eaae2d91b04d3eb6e5647b94a8b02aee50
bengunurgultekin54@gmail.com <bengunurgultekin54@gmail.com>
2020-11-10 21:58:06 +0300

    no message

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="mainContainer">
+    <TheSlider/>
+    <h2 class="coksatan">ÇOK SATAN TANIŞMA SETLERİ</h2>
+    <TheShowcase/>
+
+  </div>
+</template>
+
+<script>
+import TheSlider from "../components/TheSlider"
+import TheShowcase from "../components/TheShowcase"
+export default {
+  TheSlider,
+  TheShowcase
+}
+</script>
+
+<style>
+  .mainContainer{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    align-items: center;
+  }
+  .coksatan{
+        color: #C65800;
+        font-family: sans-serif;
+        font-weight: bold;
+    }
+</style>
